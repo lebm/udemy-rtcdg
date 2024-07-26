3fbf5c635d59cb289210c2574d3f638db899a3ec
Luis Bravo <ledbrv@gmail.com>
2024-07-26 20:07:28 -0300

    Finished ch10

--- project1/deck/src/main.rs
+++ project1/deck/src/main.rs
@@ -5,24 +5,38 @@ struct Deck {
     cards: Vec<String>,
 }
 
-fn main() {
-    let suits = ["Hearts", "Spades", "Diamonds", "Clubs"];
-    //let values = ["2", "3", "4"];
-    let values = [2, 3, 4];
-    //let deck = Deck { cards: Vec::new() };
+impl Deck {
+    // new is an associated function. It belongs to the type. Does not need an instance.
+    // In fact, new creates an instance of the type, Deck in this case.
+    // Its no associated with an instance.
+    fn new() -> Self {
+        let suits = ["Hearts", "Spades", "Diamonds", "Clubs"];
+        let values = [2, 3, 4];
 
-    let mut cards = vec![];
-    for suit in suits {
-        for value in values {
-            let card = format!("{value} of {suit}");
-            cards.push(card);
+        let mut cards = vec![];
+        for suit in suits {
+            for value in values {
+                let card = format!("{value} of {suit}");
+                cards.push(card);
+            }
         }
+
+        let deck = Deck { cards };
+        // "return deck;" is valid, but being an expression base language, a line with the value at the end is enough
+        deck
     }
 
-    // Equivalent to  let deck = Deck { cards: cards };
-    let deck = Deck { cards };
+    // shuffle is a method. Its first paramenter is a value of the type. It operates on a instance.
+    // Its is associated with an instance.
+    fn shuffle(&mut self) {
+        ()
+    }
+}
 
-    //println!("Heres your deck: {}", deck);
-    //println!("Heres your deck: {deck:?}");
+fn main() {
+    let mut deck = Deck::new();
+    println!("Heres your deck: {deck:#?}");
+    //let deck = deck.shuffle();
+    deck.shuffle();
     println!("Heres your deck: {deck:#?}");
 }
